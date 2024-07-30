a304e146138afe81522ed04ff0192454364f2acf
Subitsha <subitsha@Subitshas-MacBook-Air.local>
2024-07-30 12:53:20 -0600

    Done Dp 4

---
+++ MaximalSquare.java
@@ -0,0 +1,55 @@
+//TC - O(m * n)
+//SC - O(m * n)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = 0;
+        int[][] dp = new int[m + 1][n +1];
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j -1], dp[i -1][j-1]));
+                    
+                }
+                max = Math.max(max,dp[i][j]);
+                
+            } 
+        }
+
+        return max * max;
+    }
+}
+
+//TC - O(m * n)
+//SC - O(n)
+
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] dp = new int[n+1];
+        int max = 0;
+        for(int i = 1; i< m+1; i++) {
+            int temp = dp[0];
+            for(int j=1; j<n+1; j++) {
+                int prev = dp[j];
+                if(matrix[i-1][j-1] == '1') {
+                    dp[j] = 1+ Math.min(dp[j - 1], Math.min(dp[j], temp));
+                    max = Math.max(max, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                temp = prev;
+            }
+        }
+        return max * max;
+    }
+}
